d12575fb9750dc1d25de9866f4c70c55df9e42e4
Prasoon <prasoonpandey06@gmail.com>
2019-03-13 18:59:50 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-var actionTime = Math.floor((Math.random() * 5) + 1);
 var div = document.getElementById("rect");
-
+var startResponseTime;
+var endResponseTime;
+var active = false;
+var actionTime = Math.floor((Math.random() * 5) + 1);
 var timeInMilis = actionTime * 1000;
 
 setTimeout(changeColor, timeInMilis);
 
 function changeColor () {
-	div.style.backgroundColor = "red";
+	div.style.backgroundColor = "rgba(200, 25, 25, 0.9)";
+	startResponseTime = new Date();
+	active = true;
+}
+
+div.onclick = function () {
+	if (active) {
+		endResponseTime = new Date();
+		var responseTime = endResponseTime - startResponseTime;
+		
+		div.innerHTML = responseTime + 'ms';
+		active = false;
+	}
+}
+
+function reload() {
+	location.reload();
 }
